470456e1a02eed021a666766a43dcc4bccdc3191
Sachin <sachin.pradhan@northwestern.edu>
2018-04-10 16:09:41 -0500

    versioning css and js files to force browsers to reload assets when updated

--- ctstem_site/ctstem_app/templatetags/base_extras.py
+++ ctstem_site/ctstem_app/templatetags/base_extras.py
@@ -6,6 +6,11 @@ import json
 import datetime
 import re
 import random
+import os
+import posixpath
+import stat
+import urllib
+from django.contrib.staticfiles import finders
 
 register = template.Library()
 
@@ -23,3 +28,14 @@ def navactive(request, urls, args=None):
 @register.simple_tag
 def settings_value(name):
     return getattr(settings, name, "")
+
+
+@register.simple_tag
+def staticfile(path):
+    normalized_path = posixpath.normpath(urllib.unquote(path)).lstrip('/')
+    absolute_path = finders.find(normalized_path)
+    if not absolute_path and getattr(settings, 'STATIC_ROOT', None):
+        absolute_path = os.path.join(settings.STATIC_ROOT, path)
+    if absolute_path:
+        return '%s%s?v=%s' % (settings.STATIC_URL, path, os.stat(absolute_path)[stat.ST_MTIME])
+    return path
