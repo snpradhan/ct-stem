44beaa11ad5554dfce66ff05c15ab63022c40eab
Sachin <sachin.pradhan@northwestern.edu>
2020-03-31 11:58:52 -0500

    add timestamp to files uploaded via ckeditor

--- ctstem_site/ctstem_site/utils.py
+++ ctstem_site/ctstem_site/utils.py
@@ -1,4 +1,9 @@
 # utils.py
+import datetime
+import os
 
 def get_filename(filename):
-    return filename.upper()
+  now = datetime.datetime.now()
+  dt = now.strftime("%Y-%m-%d-%H-%M-%S-%f")
+  filename_base, filename_ext = os.path.splitext(filename)
+  return '%s_%s%s' % (filename_base.lower(), dt, filename_ext.lower(),)
